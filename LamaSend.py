b15a397d70877f96ebdf3cee695b2472a11da633
moonz <mihail.moonz@gmail.com>
2018-03-08 16:21:20 +0300

    Всё работает, но проверку реализовать не удалось =(

--- LamaSend.py
+++ LamaSend.py
@@ -5,15 +5,28 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome")
 driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options)
-driver.get("https://takadk.deviantart.com/")
+
+
+file = open('links.txt', 'r')
 
 def send():
-    time.sleep(2)
-    driver.find_element_by_xpath("//div[@id='deviant']/div/div[3]/a[3]/span").click()
-    time.sleep(2)
-    driver.find_element_by_xpath("//body[@id='deviantART-v7']/div/div/div/a[3]/span").click()
-    time.sleep(5)
-    driver.switch_to.frame('badges_iframe')
-    driver.find_element_by_xpath("//input[@value='Give']").click()
+    time.sleep(3)
+    driver.find_element_by_xpath("//img[contains(@src,'https://st.deviantart.net/minish/gallery/down.gif')]").click()
+    time.sleep(3)
+    try:
+        driver.find_element_by_xpath("//body[@id='deviantART-v7']/div/div/div/a[3]/span").click()
+        time.sleep(4)
+        driver.switch_to.frame('badges_iframe')
+        driver.find_element_by_xpath("//input[@value='Give']").click()
+    except:
+        print('пусто')
 
-send()
+
+
+while file != '':
+    a = file.readline()
+    driver.get(a)
+    try:
+        send()
+    except:
+        continue
