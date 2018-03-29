f93ae4db35042570090b4a89e17dff5b30c3b9af
Mikhail <35227454+moonzlo@users.noreply.github.com>
2018-03-29 10:34:36 +0300

    Доработанная версия, быстро и качественно )

--- VideoTimeGrab.py
+++ VideoTimeGrab.py
@@ -2,57 +2,63 @@
 
 import time
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+
+from bs4 import BeautifulSoup
+
+start_time = time.time()
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')  # Запускаем бразуер бещ GUI (без графичиской оболочки)
-options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome",)
+options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome", )
 driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options)
-
 
 driver.get("https://www.youtube.com/feed/history")
 
-def coments():
+
+def videoGrabTime():
+    for i in range(1, 1000):  # Количество прокруток, если поставить больше 1000 получаем синий экарн :3
+        driver.set_page_load_timeout(30)
+        driver.execute_script("window.scrollTo(0,100000000000000);")
+        # Скролим ДО значения которого всегда больше результата, иначе процесс будет прерван.
+        time.sleep(2)  # Нужно немножечко поспать =)
+
+    time.sleep(2)
     try:
-        action = ActionChains(driver)
-
-        time.sleep(2)  # Даём странице ПОЛНОСТЬЮ прогрузиться.
-        # Запускаем цикл прокручивания до максимального упора.
-        for i in range(1, 100):  # Количество прокруток, если поставить больше 1000 получаем синий экарн :3
-            driver.set_page_load_timeout(30)
-            driver.execute_script("window.scrollTo(0,100000000000000);")
-            # Скролим ДО значения которого всегда больше результата, иначе процесс будет прерван.
-            time.sleep(2)  # Нужно немножечко поспать =)
-
-
-        # Поиск минут видео
-        # Ищем фрэйм с цифрами
         b = driver.find_elements_by_xpath("//div[@id='overlays']/ytd-thumbnail-overlay-time-status-renderer")
-        print(len(b))
+        print('Всего роликов было найдено :', len(b))
+        html = driver.page_source  # Получаем HTML код загруженной страницы
+        spisok = []
+        summ = int()
 
-        summ = int()  # Место хранения результата.
+        pattern = r'\d'
+        soup = BeautifulSoup(html, 'lxml')  # Создаем объект класса BeautifulSoup
+        test = soup.find_all(class_='style-scope ytd-thumbnail-overlay-time-status-renderer')  # Искомое поле с цифрами
 
-        for i in b:  # Итерируемся по каждому найденному объекту.
-            try:
-                action.move_to_element(i).perform()
-                # Имитируемым наведение мышки, и действие "отпускания" =)
-                sp = i.text.split(':')
-                if len(sp) == 3:
-                    hour = int(sp[0])
-                    minut = int(sp[1])
-                    summ += hour * 60 + minut
-                    # секунды брать не будет =) мне лень :P
-                elif len(sp) == 2:
-                    minut = int(sp[0])
-                    summ += minut
-                else:  # Секундные ролики в расчет брать не будем.
-                    continue
-
-            except:  # На всякий случай )
+        for i in test:
+            a = i.text.strip()  # Отсеиваем цифры от мусора.
+            b = a.split(':')  # Разделям цифры для подсчета.
+            if len(b) == 3:
+                hour = int(b[0])
+                minut = int(b[1])
+                summ += hour * 60 + minut
+                # секунды брать не будет =) мне лень :P
+            elif len(b) == 2:
+                minut = int(b[0])
+                summ += minut
+            else:  # Секундные ролики в расчет брать не будем.
                 continue
 
-    finally:
-        driver.close()  # Обязательно выключаем веб драйвер. (иначе он виснит в процессах)
-    return print(summ)  # Возвращаем результат
+        print('Минут :', summ)
 
-coments()
+
+
+    except:
+        driver.close()
+        driver.quit()
+    finally:
+        driver.close()
+        driver.quit()
+
+
+videoGrabTime()
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
