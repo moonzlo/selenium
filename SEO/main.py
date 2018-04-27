d1e1fd1d62cc4ef95d8c19d8b0f924814b2a8fd3
Mikhail <35227454+moonzlo@users.noreply.github.com>
2018-04-27 23:23:08 +0300

    Chrome class version
    Решил всё же хром подходит лучше для моих задач, хоть и работа возможна только с HTTPS прокси.

--- SEO/main.py
+++ SEO/main.py
@@ -3,26 +3,36 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 
-PROXY = '66.70.255.195:3128' # Работает только с HTTPS
-
-options = webdriver.ChromeOptions()
-#options.add_argument('headless')  # Запускаем бразуер бещ GUI (без графичиской оболочки)
-options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome")
-options.add_argument('--proxy-server=%s' % PROXY)
-driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options)
-
-
-
+class WebDriver(object):
+    def __init__(self, proxy):
+        self.PROXY = proxy
+        options = webdriver.ChromeOptions()
+        # options.add_argument('headless')  # Запускаем бразуер бещ GUI (без графичиской оболочки)
+        options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome")
+        options.add_argument('--proxy-server=%s' % self.PROXY)
+        self.driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options)
 
 
-try:
-    driver.get("https://yandex.ru/internet/")
-    time.sleep(10)
-except Exception as error:
-    print(error)
+    def start(self):
+        try:
+            self.driver.get("https://yandex.ru/internet/")
+            time.sleep(10)
+        except Exception as error:
+            print(error)
 
-finally:
-    driver.close()
-    time.sleep(2)
-    driver.quit()
-    time.sleep(2)
+        finally:
+            self.driver.close()
+            time.sleep(2)
+            self.driver.quit()
+            time.sleep(2)
+
+
+
+
+
+
+
+
+PROXY = '167.99.158.48:443' # Работает только с HTTPS
+a = WebDriver(PROXY)
+a.start()
