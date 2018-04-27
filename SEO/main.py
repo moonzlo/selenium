93a9ef311d9ec042db1e5429a3de0a694827fc5c
Mikhail <35227454+moonzlo@users.noreply.github.com>
2018-04-27 21:41:38 +0300

    Chrome version
    Работа с хромом накладывает определенные ограничения с которыми я не могу смириться, по этому продолжаю разработку на лисе

--- SEO/main.py
+++ SEO/main.py
@@ -1,23 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.chrome.options import Options
 import time
 
 
-prox = Proxy()
-prox.proxy_type = ProxyType.MANUAL
-
-prox.ssl_proxy = "167.99.3.74:53"
-
-
-
-
-capabilities = webdriver.DesiredCapabilities.CHROME
-prox.add_to_capabilities(capabilities)
+PROXY = '66.70.255.195:3128' # Работает только с HTTPS
 
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')  # Запускаем бразуер бещ GUI (без графичиской оболочки)
 options.add_argument(r"user-data-dir=C:\Users\moonz\AppData\Local\Google\Chrome")
-driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options, desired_capabilities=capabilities)
+options.add_argument('--proxy-server=%s' % PROXY)
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=options)
 
 
 
@@ -25,8 +17,7 @@ driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options
 
 try:
     driver.get("https://yandex.ru/internet/")
-    time.sleep(100)
-    driver.get("https://metal52.ru")
+    time.sleep(10)
 except Exception as error:
     print(error)
 
