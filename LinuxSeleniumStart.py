3d874d93706a4dcd92698bb7ea83fa42852b1104
Mikhail <35227454+moonzlo@users.noreply.github.com>
2018-06-18 23:11:55 +0300

    fast start

--- LinuxSeleniumStart.py
+++ LinuxSeleniumStart.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+"""Данная конфигурация реализует базовую настройку с использованием профиля пользователя """
 
 options = webdriver.ChromeOptions()
+options.add_argument('headless')  # Без GUI
 options.add_argument(r"user-data-dir=/home/moonz/profile")
 driver = webdriver.Chrome(executable_path="/home/moonz/chromedriver", chrome_options=options)
 
