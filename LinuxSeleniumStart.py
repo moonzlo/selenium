9956a1b2a21f0d2130bbd22544a0c2c7953e64de
Mikhail <35227454+moonzlo@users.noreply.github.com>
2019-02-09 19:24:51 +0300

    server edition
    для работы на удалённом хосте

--- LinuxSeleniumStart.py
+++ LinuxSeleniumStart.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')  # Без GUI
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 options.add_argument(r"user-data-dir=/home/moonz/profile")
 driver = webdriver.Chrome(executable_path="/home/moonz/chromedriver", chrome_options=options)
 
