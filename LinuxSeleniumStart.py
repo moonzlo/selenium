5fcaedb02981eef562a3a180c1df15f2dfcb6690
Mikhail <35227454+moonzlo@users.noreply.github.com>
2019-05-30 13:22:08 +0300

    add cooke
    работы с печеньем

--- LinuxSeleniumStart.py
+++ LinuxSeleniumStart.py
@@ -8,3 +8,16 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument(r"user-data-dir=/home/moonz/profile")
 driver = webdriver.Chrome(executable_path="/home/moonz/chromedriver", chrome_options=options)
 
+
+import pickle
+"""
+Библиотека для работы с куками
+"""
+
+
+pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))  # Пишем куки в файл
+and later to add them back:
+
+cookies = pickle.load(open("cookies.pkl", "rb"))  # Читаем куки из файла
+for cookie in cookies:
+    driver.add_cookie(cookie)
